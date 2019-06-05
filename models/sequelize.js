ede0d3005e1d0e2cdc3709bb973bff4a49800c64
Douglas Henderson <douglas.henderson27@gmail.com>
2019-06-04 22:49:58 -0500

    added some more field validation to our tables

--- models/sequelize.js
+++ models/sequelize.js
@@ -7,18 +7,38 @@ var connection = new Sequelize("dungeon_db", "root", "Liam2012", {
 });
 
 var Rule = connection.define("rules", {
-  title: Sequelize.STRING,
-  description: Sequelize.TEXT
+  title: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
 });
 
 var User = connection.define("users", {
-  firebase_id: Sequelize.STRING,
-  rule_id_list: Sequelize.TEXT
+  firebase_id: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  rule_id_list: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
 });
 
 var Memo = connection.define("memo", {
-  title: Sequelize.STRING,
-  message: Sequelize.TEXT
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  message: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
 });
 
 connection.sync();

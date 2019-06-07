d38b6c917b0cef843d3193b28ad4f78a006b9132
Collin <collintegral@gmail.com>
2019-06-06 21:10:01 -0500

    Updated database model, set up api routes for communicating with database

---
+++ models/dungeonDB.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+  const dungeonDB = {
+    Rule: sequelize.define("rules", {
+      title: {
+        type: Datatypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      description: {
+        type: Datatypes.TEXT,
+        allowNull: false
+      }
+    }),
+
+    User: sequelize.define("users", {
+      firebase_id: {
+        type: Datatypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      rule_id_list: {
+        type: Datatypes.TEXT,
+        allowNull: false
+      }
+    }),
+
+    Memo: sequelize.define("memo", {
+      title: {
+        type: Datatypes.STRING,
+        allowNull: false
+      },
+      message: {
+        type: Datatypes.TEXT,
+        allowNull: false
+      },
+      userID: {
+        type: Datatypes.VARCHAR,
+      }
+    })
+  }
+  return dungeonDB;
+}

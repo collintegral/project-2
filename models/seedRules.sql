72ffb201c93cd88dd739b05f04640cf620eda383
Douglas Henderson <douglas.henderson27@gmail.com>
2019-06-11 20:31:34 -0500

    added seeds files

---
+++ models/seedRules.sql
@@ -0,0 +1,11 @@
+INSERT INTO rules
+  ( title, description, createdAt, updatedAt )
+VALUES
+  ('rule1', 'first rule', createdAt, updatedAt),
+  ('rule2', '2nd rule', createdAt, updatedAt),
+  ('rule3', '3rd rule', createdAt, updatedAt)
+
+
+
+Select *
+From rules;
---
+++ models/seedUser.sql
@@ -0,0 +1,11 @@
+INSERT INTO users
+  ( firebase_id, rule_id_list, createdAt, updatedAt )
+VALUES
+  ('1', '1', createdAt, updatedAt),
+  ('2', '1', createdAt, updatedAt),
+  ('3', '1', createdAt, updatedAt)
+
+
+
+Select *
+From users;

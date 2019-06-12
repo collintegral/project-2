b90217a19488af30e648404b6c55d688255535f2
jsmithwh <46731513+Jsmithwh@users.noreply.github.com>
2019-06-11 21:03:08 -0500

    filled seeds files for memos and rules

--- models/seedMemo.sql
+++ models/seedMemo.sql
@@ -1,9 +1,18 @@
 INSERT INTO memos
   ( title, message, user_id, createdAt, updatedAt )
 VALUES
-  ('favorite rule', 'i used this rule one time and it was cool', '1', createdAt, updatedAt),
-  ('second favorite rule', 'i used this rule when it was dark and scarry outside', '2' , createdAt, updatedAt),
-  ('A Fun Rule', 'This Rule turns everyone into a dwarf', '3' , createdAt, updatedAt)
+  ('favorite rule', 'i used this rule one time and it was cool', '0', createdAt, updatedAt),
+  ('second favorite rule', 'i used this rule when it was dark and scarry outside', '0' , createdAt, updatedAt),
+  ('Player Rule', 'Need to remember this rule for Elliot', '0', createdAt, updatedAt),
+  ('I need this one', 'For that orc I made for SOME REASON', '0' , createdAt, updatedAt),
+  ('I should not need this rule', 'This rule occurs when my players beat my puzzle', '0' , createdAt, updatedAt),
+  ('Remember this!', 'I let Winnie homebrew as a minotaur', '0', createdAt, updatedAt),
+  ('Give this to Lydia', 'Disease damage', '0' , createdAt, updatedAt),
+  ('A Fun Rule', 'This Rule turns everyone into a dwarf', '0' , createdAt, updatedAt),
+  ('Good Stuff', 'Throw this at Sunday group as revenge for last week', '0', createdAt, updatedAt),
+  ('Trap rule', 'Specifically for the half-elf', '0' , createdAt, updatedAt),
+  ('Wednesday group finale', 'Lets see them beat this', '0' , createdAt, updatedAt),
+  ('A cool Rule', 'USE THIS LATER', '0' , createdAt, updatedAt)
 
 
 
